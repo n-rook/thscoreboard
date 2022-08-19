d190a320639da160e17d3f3a6e9b19960dbf5ef4
Nathaniel Rook <nrook@nrook.com>
2022-08-18 21:32:44 -0400

    Add a logout page.

--- project/thscoreboard/users/urls.py
+++ project/thscoreboard/users/urls.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from . import views
@@ -8,7 +9,15 @@ app_name = 'users'
 urlpatterns = [
     path('register', views.register, name='register'),
     path('registration_success', views.registration_success, name='registration_success'),
-    # For now, just reuse the built-in Django pages.
-    path('', include('django.contrib.auth.urls')),
-    
+
+    path(
+        'login',
+        auth_views.LoginView.as_view(),
+        {'template_name': 'registration/login.html'},
+        name='login'),
+    path(
+        'logout',
+        auth_views.LogoutView.as_view(),
+        {'template_name': 'registration/logout2.html'},
+        name='logout'),
 ]
