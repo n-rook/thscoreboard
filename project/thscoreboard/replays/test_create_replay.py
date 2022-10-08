e9cd82da0929a88a6659012b3490ff6fd8e698e4
Nate Rook <nrook@nrook.com>
2022-10-08 19:41:29 -0400

    Extract create_replay to new module and add a test for it. (#70)

---
+++ project/thscoreboard/replays/test_create_replay.py
@@ -0,0 +1,113 @@
+from os import path
+
+from replays import models
+from replays import create_replay
+from replays import replay_parsing
+from replays.testing import test_case
+
+
+def ReadTestFile(filename):
+    with open(path.join('replays/replays_for_tests', filename), 'rb') as f:
+        return f.read()
+
+
+class GameIDsComprehensiveTestCase(test_case.ReplayTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.user = self.createUser('some-user')
+
+    def testPublishWithoutReplayFile(self):
+        shot = models.Shot.objects.get(shot_id='Mima')
+
+        new_replay = create_replay.PublishReplayWithoutFile(
+            user=self.user,
+            difficulty=1,
+            shot=shot,
+            points=10000,
+            category=models.Category.REGULAR,
+            comment='Hello',
+            video_link='https://www.youtube.com/example',
+            route=None,
+        )
+
+        self.assertEqual(new_replay.user, self.user)
+        self.assertEqual(new_replay.GetDifficultyName(), 'Normal')
+        self.assertEqual(new_replay.shot, shot)
+        self.assertEqual(new_replay.points, 10000)
+        self.assertIsNone(new_replay.rep_points)
+        self.assertEqual(new_replay.video_link, 'https://www.youtube.com/example')
+        self.assertEqual(new_replay.comment, 'Hello')
+
+        self.assertEqual(
+            new_replay,
+            models.Replay.objects.get(id=new_replay.id)
+        )
+    
+    def testPublishReplay(self):
+        replay_file_contents = ReadTestFile('th10_normal.rpy')
+        
+        temp_replay = models.TemporaryReplayFile(
+            user=self.user,
+            replay=replay_file_contents
+        )
+        temp_replay.save()
+
+        replay_info = replay_parsing.Parse(replay_file_contents)
+
+        shot = models.Shot.objects.get(game='th10', shot_id='ReimuB')
+
+        new_replay = create_replay.PublishNewReplay(
+            user=self.user,
+            difficulty=3,
+            shot=shot,
+            points=294127890,
+            category=models.Category.REGULAR,
+            comment='Hello',
+            video_link='',
+            is_good=True,
+            temp_replay_instance=temp_replay,
+            replay_info=replay_info,
+        )
+
+        self.assertEqual(new_replay.user, self.user)
+        self.assertEqual(new_replay.GetDifficultyName(), 'Lunatic')
+        self.assertEqual(new_replay.shot, shot)
+        self.assertEqual(new_replay.points, 294127890)
+        self.assertEqual(new_replay.rep_points, 294127890)
+        self.assertEqual(new_replay.category, models.Category.REGULAR)
+        self.assertEqual(new_replay.comment, 'Hello')
+
+        with self.assertRaises(models.TemporaryReplayFile.DoesNotExist):
+            models.TemporaryReplayFile.objects.get(id=temp_replay.id)
+
+    def testPublishReplaySavesStages_TH10(self):
+        replay_file_contents = ReadTestFile('th10_normal.rpy')
+
+        temp_replay = models.TemporaryReplayFile(
+            user=self.user,
+            replay=replay_file_contents
+        )
+        temp_replay.save()
+
+        replay_info = replay_parsing.Parse(replay_file_contents)
+
+        shot = models.Shot.objects.get(game='th10', shot_id='ReimuB')
+
+        new_replay = create_replay.PublishNewReplay(
+            user=self.user,
+            difficulty=3,
+            shot=shot,
+            points=294127890,
+            category=models.Category.REGULAR,
+            comment='',
+            video_link='',
+            is_good=True,
+            temp_replay_instance=temp_replay,
+            replay_info=replay_info,
+        )
+
+        stages = list(models.ReplayStage.objects.filter(replay=new_replay))
+        self.assertEqual(len(stages), 6)
+
+        self.assertEqual(stages[1].score, 12996310)
