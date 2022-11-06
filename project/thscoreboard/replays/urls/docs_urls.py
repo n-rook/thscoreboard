4029a1d104fd24558d95f2793e2e8c601ed8c5c9
Nate Rook <nrook@nrook.com>
2022-11-06 16:22:57 -0500

    Add a privacy policy. (#93)

--- project/thscoreboard/replays/urls/docs_urls.py
+++ project/thscoreboard/replays/urls/docs_urls.py
@@ -5,5 +5,6 @@ from replays.views import docs
 
 urlpatterns = [
     path('', docs.make_docs_page_route('about.html'), name='Docs/About'),
-    path('terms', docs.make_docs_page_route('terms_of_use.html'), name='Docs/TermsOfUse')
+    path('terms', docs.make_docs_page_route('terms_of_use.html'), name='Docs/TermsOfUse'),
+    path('privacy', docs.make_docs_page_route('privacy_policy.html'), name='Docs/Privacy'),
 ]
