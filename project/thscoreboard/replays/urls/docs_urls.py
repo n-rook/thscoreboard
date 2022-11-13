bc58b7154b656f65acf53f71a7b1b5554b642b4f
Nathaniel Rook <nrook@nrook.com>
2022-11-13 12:55:16 -0500

    Add a main site rules page.

--- project/thscoreboard/replays/urls/docs_urls.py
+++ project/thscoreboard/replays/urls/docs_urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', docs.make_docs_page_route('about.html'), name='Docs/About'),
     path('terms', docs.make_docs_page_route('terms_of_use.html'), name='Docs/TermsOfUse'),
     path('privacy', docs.make_docs_page_route('privacy_policy.html'), name='Docs/Privacy'),
+    path('rules', docs.make_docs_page_route('rules.html'), name='Docs/Rules'),
     path('replayrules', docs.make_docs_page_route('replay_rules.html'), name='Docs/Replay Rules'),
 ]
