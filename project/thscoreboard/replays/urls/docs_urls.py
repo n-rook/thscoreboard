306228dceec6c2be34631efbc3ad3ba729faa44c
Nate Rook <nrook@nrook.com>
2022-11-13 11:10:22 -0500

    Add some basic replay rules. #52 (#96)

--- project/thscoreboard/replays/urls/docs_urls.py
+++ project/thscoreboard/replays/urls/docs_urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', docs.make_docs_page_route('about.html'), name='Docs/About'),
     path('terms', docs.make_docs_page_route('terms_of_use.html'), name='Docs/TermsOfUse'),
     path('privacy', docs.make_docs_page_route('privacy_policy.html'), name='Docs/Privacy'),
+    path('replayrules', docs.make_docs_page_route('replay_rules.html'), name='Docs/Replay Rules'),
 ]
