4e83ba408a1b4a737c8e5625793ad6e7d5450be5
32th-System <intensonat@gmail.com>
2022-11-14 18:38:55 +0100

    email: determine site URL with request.META

--- project/thscoreboard/users/send_email.py
+++ project/thscoreboard/users/send_email.py
@@ -1,41 +1,17 @@
 """Contains functions that send specific email messages."""
-from urllib import parse
-
 
 from django.core import mail
-from django.contrib.sites.shortcuts import get_current_site
 from django.template import loader
 from django import urls
 
-from thscoreboard import settings
 from . import models
 
 
 _ACCOUNTS_EMAIL = 'Silent Selene Accounts <accounts@silentselene.net>'
 
 
-def _GetFullyQualifiedLink(request, url_path: str):
-    site = get_current_site(request)
-    if settings.USE_HTTPS_IN_EMAIL_LINKS:
-        scheme = 'https'
-    else:
-        scheme = 'http'
-
-    return parse.urlunparse((
-        scheme,
-        site.domain,
-        url_path,
-        None,
-        None,
-        None
-    ))
-
-
 def SendVerificationEmail(request, u: models.UnverifiedUser):
-    full_link = _GetFullyQualifiedLink(
-        request,
-        urls.reverse('users:verify_email', kwargs={'token': u.token})
-    )
+    full_link = request.META['HTTP_ORIGIN'] + urls.reverse('users:verify_email', kwargs={'token': u.token})
 
     text_message = loader.render_to_string(
         'registration/email/verification.txt',
