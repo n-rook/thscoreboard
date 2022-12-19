2d0cac7597e30643c18215cd6ae2213d27b8bee6
32th System <intensonat@gmail.com>
2022-12-19 01:01:27 +0100

    Dark theme (#149)

---
+++ project/thscoreboard/shared_content/static/js/main.js
@@ -0,0 +1,25 @@
+document.body.classList.remove("theme-light");
+
+const themes = [ "theme-light", "theme-dark" ];
+
+let theme = localStorage.getItem("theme");
+if(!themes.includes(theme)) {
+    theme = "theme-light";
+    localStorage.setItem("theme", theme);
+}
+
+document.body.classList.add(theme);
+
+const toggleTheme = () => {
+    document.body.classList.remove(theme)
+    switch(theme) {
+    case "theme-light":
+        theme = "theme-dark";
+        break;
+    case "theme-dark": default:
+        theme = "theme-light"
+        break;
+    }
+    document.body.classList.add(theme);
+    localStorage.setItem("theme", theme);
+}
