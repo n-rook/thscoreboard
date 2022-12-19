dd836314e5dc19112a90d63f7aa410cfeb1cd4ce
raviddog <raviddog@outlook.com>
2022-12-19 13:04:53 +1100

    add db constraint violation tests

--- project/thscoreboard/replays/test_create_replay.py
+++ project/thscoreboard/replays/test_create_replay.py
@@ -6,6 +6,8 @@ from replays import replay_parsing
 from replays.testing import test_case
 from replays.testing import test_replays
 
+import django.db.utils
+
 
 class GameIDsComprehensiveTestCase(test_case.ReplayTestCase):
 
@@ -138,3 +140,49 @@ class GameIDsComprehensiveTestCase(test_case.ReplayTestCase):
             temp_replay_instance=temp_replay,
             replay_info=replay_info,
         )
+
+    def testReplayTypeConstraint(self):
+
+        shot = models.Shot.objects.get(game_id=game_ids.GameIDs.TH05, shot_id='Mima')
+
+        with self.assertRaises(django.db.utils.IntegrityError):
+            create_replay.PublishReplayWithoutFile(
+                user=self.user,
+                difficulty=1,
+                shot=shot,
+                score=10000,
+                category=models.Category.REGULAR,
+                comment='Hello',
+                is_clear=True,
+                video_link='https://www.youtube.com/example',
+                route=None,
+                replay_type=game_ids.ReplayTypes.SPELL_PRACTICE,
+            )
+
+    def testReplayTypeConstraint2(self):
+        replay_file_contents = test_replays.GetRaw('th8_spell_practice')
+
+        temp_replay = models.TemporaryReplayFile(
+            user=self.user,
+            replay=replay_file_contents
+        )
+        temp_replay.save()
+        replay_info = replay_parsing.Parse(replay_file_contents)
+        shot = models.Shot.objects.get(game=replay_info.game, shot_id=replay_info.shot)
+        
+        replay_info.replay_type = game_ids.ReplayTypes.REGULAR
+
+        with self.assertRaises(django.db.utils.IntegrityError):
+            create_replay.PublishNewReplay(
+                user=self.user,
+                difficulty=replay_info.difficulty,
+                shot=shot,
+                score=replay_info.score,
+                category=models.Category.REGULAR,
+                comment='',
+                video_link='',
+                is_good=True,
+                is_clear=True,
+                temp_replay_instance=temp_replay,
+                replay_info=replay_info,
+            )
