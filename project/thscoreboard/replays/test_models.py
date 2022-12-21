e619d1b647b7a58fe8389df73ca9beff7331dfcd
raviddog <raviddog@outlook.com>
2022-12-21 15:48:05 +1100

    remove unneeded test

--- project/thscoreboard/replays/test_models.py
+++ project/thscoreboard/replays/test_models.py
@@ -103,28 +103,3 @@ class TestConstraints(test_case.ReplayTestCase):
                 temp_replay_instance=temp_replay,
                 replay_info=replay_info,
             )
-
-    def testSpellCardName(self):
-        replay_file_contents = test_replays.GetRaw('th8_spell_practice')
-
-        temp_replay = models.TemporaryReplayFile(
-            user=self.user,
-            replay=replay_file_contents
-        )
-        temp_replay.save()
-        replay_info = replay_parsing.Parse(replay_file_contents)
-        shot = models.Shot.objects.get(game=replay_info.game, shot_id=replay_info.shot)
-        
-        create_replay.PublishNewReplay(
-            user=self.user,
-            difficulty=replay_info.difficulty,
-            shot=shot,
-            score=replay_info.score,
-            category=models.Category.REGULAR,
-            comment='',
-            video_link='',
-            is_good=True,
-            is_clear=True,
-            temp_replay_instance=temp_replay,
-            replay_info=replay_info,
-        )
