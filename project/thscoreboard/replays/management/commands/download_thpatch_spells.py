f68ea9bce2ae01a328d78a5cd50c67052621ab73
32th-System <intensonat@gmail.com>
2022-12-22 22:12:41 +0100

    Add download_thpatch_spells.py
    This reverts commit db38523e0ff695cd9672708449d5eddad3088387.

---
+++ project/thscoreboard/replays/management/commands/download_thpatch_spells.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+import os
+import requests
+import replays.spell_cards
+
+
+class Command(BaseCommand):
+    
+    help = 'Download spell names from thpatch.net'
+
+    def handle(self, *args, **kwargs):
+
+        for lang in replays.spell_cards.thcrap_langs:
+            for game in replays.spell_cards.has_thcrap:
+                os.makedirs(f'replays/spells/{lang}', exist_ok=True)
+                with open(f'replays/spells/{lang}/{game}.json', 'wb') as f:
+                    f.write(requests.get(f'https://srv.thpatch.net/lang_{lang}/{game}/spells.js').content)
