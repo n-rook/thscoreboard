ffa82c278d6f8de0432a0cea42ff95873d91416a
Nate Rook <nrook@nrook.com>
2023-01-19 09:44:21 -0500

    Add a function that reanalyzes all replays. (#193)
    * Add a bool function that checks if a replay has a diff.
    
    This wound up not being useful to me.
    
    * Add a view that can reanalyze all replays!

--- project/thscoreboard/replays/urls/replay_urls.py
+++ project/thscoreboard/replays/urls/replay_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from replays.views import create_replay
 from replays.views import index
+from replays.views import reanalyze_all_replays
 from replays.views import replay_list
 from replays.views import user
 from replays.views import view_replay
@@ -13,6 +14,7 @@ urlpatterns = [
     path('publish/<int:temp_replay_id>', create_replay.publish_replay),
     path('publish/<str:game_id>', create_replay.publish_replay_no_file),
     path('user/<str:username>', user.user_page),
+    path('reanalyze_all', reanalyze_all_replays.reanalyze_all),
     path('<str:game_id>', replay_list.game_scoreboard),
     path('<str:game_id>/d<int:difficulty>', replay_list.game_scoreboard),
     path('<str:game_id>/d<int:difficulty>/<str:shot_id>', replay_list.game_scoreboard),
@@ -20,5 +22,5 @@ urlpatterns = [
     path('<str:game_id>/<int:replay_id>/edit', view_replay.edit_replay),
     path('<str:game_id>/<int:replay_id>/download', view_replay.download_replay),
     path('<str:game_id>/<int:replay_id>/delete', view_replay.delete_replay),
-    path('<str:game_id>/<int:replay_id>/reanalyze', view_replay.view_replay_reanalysis),
+    path('<str:game_id>/<int:replay_id>/reanalyze', view_replay.view_replay_reanalysis, name='Replays/Reanalysis'),
 ]
