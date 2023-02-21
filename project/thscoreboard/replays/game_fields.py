24bc6fb315c0e25e66e542424f07ff962606705a
jgdhs27 <markusobendrauf@gmail.com>
2023-02-21 22:10:33 +0100

    Fix SA Marisa A power display

--- project/thscoreboard/replays/game_fields.py
+++ project/thscoreboard/replays/game_fields.py
@@ -2,8 +2,9 @@
 
 import copy
 from immutabledict import immutabledict
-from typing import Optional
+from typing import Iterable, Optional
 from . import game_ids
+from replays import models
 
 #   These table fields configure the display of the stage split columns
 #   These may differ from whether the replay file actually has the field or not
@@ -341,11 +342,13 @@ _game_fields_PVP = immutabledict({
 })
 
 
-def GetFormatPower(game_id: str, power: Optional[int]) -> str:
+def GetFormatPower(game_id: str, power: Optional[int], shot_name: Optional[str] = None) -> str:
     if power is None:
         return ""
     if game_id in (game_ids.GameIDs.TH06, game_ids.GameIDs.TH07, game_ids.GameIDs.TH08):
         return str(power)
+    if game_id == game_ids.GameIDs.TH11 and shot_name == "MarisaA":
+        return "%.2f" % (float(power) / 12)
     if game_id in (game_ids.GameIDs.TH10, game_ids.GameIDs.TH11):
         return "%.2f" % (float(power) * 0.05)
     if game_id in (
@@ -491,12 +494,12 @@ def GetFormatStage(game_id: str, stage: Optional[int]) -> str:
     return str(stage)
 
 
-def FormatStages(game_id: str, replay_stages):
+def FormatStages(game_id: str, replay_stages: Iterable[models.ReplayStage], shot_name: str):
     """This function formats the stage values to be displayed in the front end"""
     new_stages = copy.deepcopy(replay_stages)
 
     for stage in new_stages:
-        stage.power = GetFormatPower(game_id, stage.power)
+        stage.power = GetFormatPower(game_id, stage.power, shot_name)
         stage.stage = GetFormatStage(game_id, stage.stage)
         stage.lives = GetFormatLives(game_id, stage.lives, stage.life_pieces, stage.extends)
         stage.bombs = GetFormatBombs(game_id, stage.bombs, stage.bomb_pieces)
