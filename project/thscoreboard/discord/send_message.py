c5e4df972a52ad227b7450d279f0f3f2f45f0be3
jgdhs27 <markusobendrauf@gmail.com>
2023-05-18 14:54:25 +0200

    Create script to send discord messages

---
+++ project/thscoreboard/discord/send_message.py
@@ -0,0 +1,29 @@
+import os
+from discord import Client, Intents
+
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+DISCORD_BOT_CHANNEL_ID = os.getenv("DISCORD_BOT_CHANNEL_ID")
+
+
+def send_message(message: str) -> None:
+    client = Client(intents=Intents.default())
+
+    @client.event
+    async def on_ready():
+        channel = client.get_channel(int(DISCORD_BOT_CHANNEL_ID))
+        await channel.send(message)
+        await client.close()
+
+    client.run(DISCORD_BOT_TOKEN)
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    from pathlib import Path
+
+    dotenv_path = Path(__file__).parent.parent / ".env"
+    load_dotenv(dotenv_path)
+    DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+    DISCORD_BOT_CHANNEL_ID = os.getenv("DISCORD_BOT_CHANNEL_ID")
+
+    send_message("Test")
