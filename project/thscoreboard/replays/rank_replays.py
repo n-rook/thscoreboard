3e11fbb9b951ff7b1976cc90fc2afa00df77363b
jgdhs27 <markusobendrauf@gmail.com>
2023-08-06 17:51:12 +0200

    Extract add_global_rank_annotations to new file

---
+++ project/thscoreboard/replays/rank_replays.py
@@ -0,0 +1,20 @@
+from replays import models
+from replays.models import ReplayQuerySet
+
+
+def add_global_rank_annotations(replays: ReplayQuerySet) -> None:
+    """Ranks replays in the replay query set against all ranked replays"""
+    top_3_replays = (
+        models.Replay.objects.filter(category=models.Category.STANDARD)
+        .filter(replay_type=1)
+        .filter(is_listed=True)
+        .annotate_with_rank()
+        .filter(rank__lte=3)
+        .all()
+    )
+    rank_dict = {replay.id: replay.rank for replay in top_3_replays}
+
+    for replay in replays:
+        replay.rank = rank_dict.get(replay.id, -1)
+
+    return replays
---
+++ project/thscoreboard/replays/test_rank_replays.py
@@ -0,0 +1,54 @@
+from unittest.mock import patch
+
+from replays.rank_replays import add_global_rank_annotations
+from replays.testing import test_case
+from replays import models
+from replays.testing import test_replays
+
+
+class GlobalRankAnnotationsTestCase(test_case.ReplayTestCase):
+    def setUp(self):
+        super().setUp()
+        self.user = self.createUser("some-user")
+
+    def _create_n_replays(self, n: int, is_tas: bool = False) -> None:
+        for i in range(n):
+            with patch(
+                "replays.constant_helpers.CalculateReplayFileHash"
+            ) as mocked_hash:
+                mocked_hash.return_value = bytes(i)
+                test_replays.CreateAsPublishedReplay(
+                    "th10_normal",
+                    user=self.user,
+                    score=1_000_000_000 - 100_000_000 * i,
+                    difficulty=0,
+                    category=models.Category.TAS
+                    if is_tas
+                    else models.Category.STANDARD,
+                )
+
+    def testRanksAgainstAllReplays(self):
+        self._create_n_replays(3)
+        replay_subset = models.Replay.objects.order_by("-score")[1:]
+        add_global_rank_annotations(replay_subset)
+
+        self.assertEqual(replay_subset[0].rank, 2)
+        self.assertEqual(replay_subset[1].rank, 3)
+
+    def testGivesNegativeRankToReplaysOutsideTopThree(self):
+        self._create_n_replays(5)
+        replay_subset = models.Replay.objects.order_by("-score")[3:]
+        add_global_rank_annotations(replay_subset)
+
+        self.assertEqual(replay_subset[0].rank, -1)
+        self.assertEqual(replay_subset[1].rank, -1)
+
+    def testIgnoresUnrankedGlobalReplays(self):
+        self._create_n_replays(3, is_tas=True)
+        test_replays.CreateAsPublishedReplay(
+            "th10_normal", user=self.user, score=0, difficulty=0
+        )
+        replay_subset = models.Replay.objects.order_by("-score")[3:]
+        add_global_rank_annotations(replay_subset)
+
+        self.assertEqual(replay_subset[0].rank, 1)
