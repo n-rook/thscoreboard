79af43b6e818b62166666c851bc2d01b9bdc7524
Nathaniel Rook <nrook@nrook.com>
2025-05-10 12:49:17 -0400

    Appropriately format dates for JP locale.

--- project/thscoreboard/replays/lib/time.py
+++ project/thscoreboard/replays/lib/time.py
@@ -2,6 +2,9 @@
 
 import datetime
 
+from django.utils import formats
+from django.utils import translation
+
 
 def strptime(date_string: str, format: str) -> datetime.datetime:
     """Returns a datetime created from a string input.
@@ -12,3 +15,21 @@ def strptime(date_string: str, format: str) -> datetime.datetime:
     return datetime.datetime.strptime(date_string, format).replace(
         tzinfo=datetime.timezone.utc
     )
+
+
+def format_month_day(timestamp: datetime.date | datetime.datetime | str) -> str:
+    """Format a date, including only the month and day."""
+    if translation.get_language() == "ja":
+        f = "n月 j日"
+    else:  # en
+        f = "d F"
+    return formats.date_format(timestamp, format=f)
+
+
+def format_date(timestamp: datetime.date | datetime.datetime | str) -> str:
+    """Format a date."""
+    if translation.get_language() == "ja":
+        f = "Y年 n月 j日"
+    else:  # en
+        f = "d F Y"
+    return formats.date_format(timestamp, format=f)
