51c98efd3ec70e2e2548812ac199f3e17bbab27d
Nate Rook <nrook@nrook.com>
2025-05-10 17:01:55 -0400

    Appropriately format dates for JP locale. (#525)
    * Appropriately format dates for JP locale.
    
    * Remove unused import

--- project/thscoreboard/replays/lib/test_time.py
+++ project/thscoreboard/replays/lib/test_time.py
@@ -1,12 +1,41 @@
 import datetime
 import unittest
 
+from django import test
+
 from replays.lib import time
 
 
 class TimeTest(unittest.TestCase):
-    def testStrptime(self):
+    def test_strptime(self):
         parsed_time = time.strptime("11/24/2001", "%m/%d/%Y")
         self.assertEqual(
             parsed_time, datetime.datetime(2001, 11, 24, tzinfo=datetime.timezone.utc)
+        )
+
+    def test_format_date_ja(self):
+        self.enterContext(test.override_settings(LANGUAGE_CODE="ja"))
+        self.assertEqual(
+            time.format_date(datetime.datetime(year=2024, month=5, day=4)),
+            "2024年 5月 4日",
+        )
+
+    def test_format_date_en(self):
+        self.enterContext(test.override_settings(LANGUAGE_CODE="en"))
+        self.assertEqual(
+            time.format_date(datetime.datetime(year=2024, month=5, day=4)),
+            "04 May 2024",
+        )
+
+    def test_format_month_day_ja(self):
+        self.enterContext(test.override_settings(LANGUAGE_CODE="ja"))
+        self.assertEqual(
+            time.format_month_day(datetime.datetime(year=2024, month=5, day=4)), "5月 4日"
+        )
+
+    def test_format_month_day_en(self):
+        self.enterContext(test.override_settings(LANGUAGE_CODE="en"))
+        self.assertEqual(
+            time.format_month_day(datetime.datetime(year=2024, month=5, day=4)),
+            "04 May",
         )
--- project/thscoreboard/replays/models.py
+++ project/thscoreboard/replays/models.py
@@ -6,7 +6,6 @@ from typing import Optional
 
 from django.db import models
 from django.db import utils
-from django.utils import formats
 from django.utils import timezone
 from django.utils.translation import pgettext_lazy
 from django.db.models import Q, QuerySet
@@ -14,6 +13,7 @@ from django.db.models import Q, QuerySet
 from replays import game_ids
 from replays import limits
 from replays import replay_parsing
+from replays.lib import time as time_lib
 from shared_content import db_errors
 from shared_content import model_ttl
 from thscoreboard import settings
@@ -449,11 +449,9 @@ class Replay(models.Model):
             return None
 
         if self.shot.game_id == game_ids.GameIDs.TH07:
-            fmt = "d F"
+            return time_lib.format_month_day(self.timestamp)
         else:
-            fmt = "d F Y"
-
-        return formats.date_format(self.timestamp, format=fmt)
+            return time_lib.format_date(self.timestamp)
 
 
 class ReplayRank(models.Model):
