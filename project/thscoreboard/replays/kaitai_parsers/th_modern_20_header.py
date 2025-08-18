ba25d5afe4a97b7746e2976e58481dac4e49574d
Nathaniel Rook <nrook@nrook.com>
2025-08-17 20:04:01 -0400

    Loosen parsing around "Chara" userdata field.

--- project/thscoreboard/replays/kaitai_parsers/th_modern_20_header.py
+++ project/thscoreboard/replays/kaitai_parsers/th_modern_20_header.py
@@ -18,24 +18,6 @@ class ThModern20Header(KaitaiStruct):
         self.main = ThModern20Header.Main(self._io, self, self._root)
         self.userdata = ThModern20Header.Userdata(self._io, self, self._root)
 
-    class Main(KaitaiStruct):
-        def __init__(self, _io, _parent=None, _root=None):
-            self._io = _io
-            self._parent = _parent
-            self._root = _root if _root else self
-            self._read()
-
-        def _read(self):
-            self.magic_ver = self._io.read_u4le()
-            self.version = self._io.read_u4le()
-            self.unused_1 = self._io.read_u4le()
-            self.userdata_offset = self._io.read_u4le()
-            self.unused_2 = self._io.read_bytes(24)
-            self.comp_size = self._io.read_u4le()
-            self.size = self._io.read_u4le()
-            self.comp_data = self._io.read_bytes(self.comp_size)
-
-
     class Userdata(KaitaiStruct):
         def __init__(self, _io, _parent=None, _root=None):
             self._io = _io
@@ -61,11 +43,29 @@ class ThModern20Header(KaitaiStruct):
             self.user_ver = ThModern20Header.Crlfstring(self._io, self, self._root)
             self.name = ThModern20Header.UserdataField(self._io, self, self._root)
             self.date = ThModern20Header.UserdataField(self._io, self, self._root)
-            self.shot = ThModern20Header.UserdataField(self._io, self, self._root)
+            self.shot = ThModern20Header.CharaUserdataField(self._io, self, self._root)
             self.difficulty = ThModern20Header.UserdataField(self._io, self, self._root)
             self.stage = ThModern20Header.Crlfstring(self._io, self, self._root)
             self.score = ThModern20Header.UserdataField(self._io, self, self._root)
             self.slowdown = ThModern20Header.UserdataField(self._io, self, self._root)
+
+
+    class Main(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.magic_ver = self._io.read_u4le()
+            self.version = self._io.read_u4le()
+            self.unused_1 = self._io.read_u4le()
+            self.userdata_offset = self._io.read_u4le()
+            self.unused_2 = self._io.read_bytes(24)
+            self.comp_size = self._io.read_u4le()
+            self.size = self._io.read_u4le()
+            self.comp_data = self._io.read_bytes(self.comp_size)
 
 
     class Crlfstring(KaitaiStruct):
@@ -77,6 +77,19 @@ class ThModern20Header(KaitaiStruct):
 
         def _read(self):
             self.value = (self._io.read_bytes_term(13, False, True, True)).decode(u"SJIS")
+            self.term = self._io.read_u1()
+
+
+    class CharaUserdataField(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.name = (self._io.read_bytes_term(32, False, True, True)).decode(u"ASCII")
+            self.value = self._io.read_bytes_term(13, False, True, True)
             self.term = self._io.read_u1()
 
 
