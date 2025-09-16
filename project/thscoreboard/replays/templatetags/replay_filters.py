38dba46aaa34345fd5c43164203ca13c43b8fd00
wefma <wefmaika1985@gmail.com>
2025-09-16 13:18:58 +0000

    does not use bare except

--- project/thscoreboard/replays/templatetags/replay_filters.py
+++ project/thscoreboard/replays/templatetags/replay_filters.py
@@ -10,8 +10,10 @@ def convert_th13_trance(value) -> str:
         return ""
 
     try:
-        gauge_units = int(value) // 200
-        gauge_progress = int(value) % 200
-        return str(gauge_units) + " + " + str(gauge_progress) + "/200"
-    except:
+        int(value)
+    except (ValueError, TypeError):
         return "invalid trance value"
+
+    gauge_units = int(value) // 200
+    gauge_progress = int(value) % 200
+    return str(gauge_units) + " + " + str(gauge_progress) + "/200"
