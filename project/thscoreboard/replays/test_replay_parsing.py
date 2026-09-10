607c0c9d70954eff1b856c2e7b3adc4d6f622a96
raviddog <raviddog@outlook.com>
2026-09-10 23:21:02 +1000

    black

--- project/thscoreboard/replays/test_replay_parsing.py
+++ project/thscoreboard/replays/test_replay_parsing.py
@@ -647,6 +647,7 @@ class Th06CReplayTestCase(unittest.TestCase):
         r = ParseTestReplay("th6c_lunatic")
         self.assertIsNotNone(r)
 
+
 class Th06NCReplayTestCase(unittest.TestCase):
     def testSpellPractice(self) -> None:
         r = ParseTestReplay("th6nc_spell_practice")
