0798e6a7ace52e97be2fb11aca0d6b692e5eb16b
nit-alcostg <unipurare1525@gmail.com>
2026-09-16 01:22:41 +0900

    normalized spell card id

--- project/thscoreboard/replays/replay_parsing.py
+++ project/thscoreboard/replays/replay_parsing.py
@@ -260,7 +260,8 @@ def _Parse06nc(rep_raw):
             slowdown=replay.file_header.slowdown,
             replay_type=r_type,
             version="1.15",
-            spell_card_id=rep_raw[8],
+            # spell card id starts with 1
+            spell_card_id=rep_raw[8] - 1,
         )
 
     enumerated_non_dummy_stages = [
